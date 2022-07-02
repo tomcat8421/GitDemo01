9028ab558101f9436da3f0e89aa78b96b7d004e8
liushuai <12346666@qq.com>
2022-07-02 21:21:20 +0800

    first commit

---
+++ src/main/java/com/javasm/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.javasm.git;
+
+/**
+ * @Author：liu帅
+ * @Version：1.0
+ * @Date：2022/7/2-21:15
+ * @Since:jdk1.8
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
